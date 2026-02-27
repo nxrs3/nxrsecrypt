628c9eed00929db781b609804a2988fe37a0071c
nxrs3 <nxrs3@proton.me>
2026-02-27 21:51:06 +0100

    Change __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,4 +2,4 @@ from .cli_system import *
 from .encryption import *
 from .map_generator import *
 
-__all__ = ['error', 'success', 'info', 'encrypt', 'decrypt',]
+__all__ = ['error', 'success', 'info', 'clear_terminal', 'load_config', 'command', 'display_command_overview', 'encrypt', 'decrypt', 'load_mapping', 'generate_map', 'make_char_list']
