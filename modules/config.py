352334f111a305f329c49115db1b07f66106982d
nxrs3 <nxrs3@proton.me>
2026-03-07 21:15:14 +0100

    Porting back to before emergency config reset

--- modules/config.py
+++ modules/config.py
@@ -6,43 +6,33 @@
 
 import os
 from getpass import getuser
-from . import config_mgmt
 
-try:
-    # directory & file locations | only edit this if your nxrsecrypt dir is actually any other place than ~/
-    home = os.path.expanduser("~")
-    location = os.path.join(home, "nxrsecrypt")
-    main_py_location = os.path.join(location, "main.py")
-    data_location = os.path.join(location, "data")
-    em_location = os.path.join(data_location, "encryption_mappings")
-    char_list_location = os.path.join(em_location, "character_lists")
-    example_em_location = os.path.join(data_location, "example_em")
-    default_cl_location = os.path.join(data_location, "default_cl")
-    default_config_location = os.path.join(data_location, "default_config")
-    config_location = os.path.join(location, "config")
-    config_module_location = os.path.join(location, "modules", "config.py")
+# directory & file locations | only edit this if your nxrsecrypt dir is actually any other place than ~/
+home = os.path.expanduser("~")
+location = os.path.join(home, "nxrsecrypt")
+main_py_location = os.path.join(location, "main.py")
+data_location = os.path.join(location, "data")
+em_location = os.path.join(data_location, "encryption_mappings")
+char_list_location = os.path.join(em_location, "character_lists")
+example_em_location = os.path.join(data_location, "example_em")
+default_cl_location = os.path.join(data_location, "default_cl")
+default_config_location = os.path.join(data_location, "default_config")
+config_location = os.path.join(location, "config")
+config_module_location = os.path.join(location, "modules", "config.py")
 
-    # program information | editing correctly wont break anything, edit if you want
-    version = "beta"
+# program information | editing correctly wont break anything, edit if you want
+version = "beta"
 
-    # other information | editing correctly wont break anything, edit if you want
-    user = getuser()
+# other information | editing correctly wont break anything, edit if you want
+user = getuser()
 
-    # customization | feel free to edit, as long as you know rich's color system
-    error_color = "bold red"
-    success_color = "bold green"
-    info_color = "italic blue"
-    headline_color = "bold cyan"
-    result_color = "bold blue"
-    command_prompt_color = "cyan"
-    welcome_message = True
-    headline_text = f"--- nxrseCrypt - v-{version} ---"
-    command_prompt = f"{user} - nxrsecrypt"
-
-except Exception:
-    location = os.path.join(os.path.expanduser("~"), "nxrsecrypt")
-    default_config_location = os.path.join(location, "data", "default_config")
-    config_module_location = os.path.join(location, "data", "default_config")
-    user_config_location = os.path.join(location, "config")
-    
-    config_mgmt.emergency_config_reset(default_config_location, config_module_location, user_config_location)
+# customization | feel free to edit, as long as you know rich's color system
+error_color = "bold red"
+success_color = "bold green"
+info_color = "italic blue"
+headline_color = "bold cyan"
+result_color = "bold blue"
+command_prompt_color = "cyan"
+welcome_message = True
+headline_text = f"--- nxrseCrypt - v-{version} ---"
+command_prompt = f"{user} - nxrsecrypt"
--- modules/config_mgmt.py
+++ modules/config_mgmt.py
@@ -1,5 +1,6 @@
+from modules import cli_system
+
 def replace_value(file_path, config_variable, new_value):
-    from . import cli_system # inside function to avoid circular impot !!
     with open(file_path, "r") as f:
         lines = f.readlines()
 
@@ -8,7 +9,7 @@ def replace_value(file_path, config_variable, new_value):
     for i, line in enumerate(lines):
         name, sep, value = line.partition("=")
         if name.strip() == config_variable:
-            lines[i] = f"    {name.strip()} = {new_value}\n"
+            lines[i] = f"{name.strip()} = {new_value}\n"
             found = True
             break
 
@@ -18,16 +19,4 @@ def replace_value(file_path, config_variable, new_value):
 
     with open(file_path, "w") as f:
         f.writelines(lines)
-    cli_system.success(f"set variable '{config_variable}' to '{new_value}'")
-
-def emergency_config_reset(default_conf_path, config_module_path, user_config_path):
-    from . import cli_system # inside function to avoid circular impot !!
-    cli_system.error("error in config, resetting to default config")
-
-    with open(default_conf_path, "r") as f:
-        default_config = f.read()
-
-    with open(config_module_path, "w") as f:
-        f.write(default_config)
-    with open(user_config_path, "w") as f:
-        f.write(default_config)
+    cli_system.success(f"set variable '{config_variable} to '{new_value}'")
